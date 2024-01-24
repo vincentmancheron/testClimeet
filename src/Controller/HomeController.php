c10ab0d4366249a6c91e1d8aa3a1f8a8609badb1
Vincent Mancheron <vmancheron@yahoo.fr>
2024-01-24 18:31:38 +0100

    Fix EventSubscriber + HomeController + CORS

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController
 {
@@ -12,7 +14,18 @@ class HomeController
     {
 
         return new Response(
-            "<html><body>Bienvenue sur l'Api testClimeet</body></html>"
+            `<html>
+                <body>
+                    <h2>Bienvenue sur l'Api testClimeet</h2>
+                    <p>Munissez-vous d'un token pour exploitation (/login_check)</p>
+                </body>
+            </html>`
         );
+    }
+
+    #[Route('/checkBearer', 'app_checkBearer', methods: ['GET'])]
+    public function checkBearer(): JsonResponse
+    {
+        return new JsonResponse('authenticated', Response::HTTP_OK, [], true);
     }
 }
--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -5,6 +5,7 @@ namespace App\EventSubscriber;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ExceptionSubscriber implements EventSubscriberInterface
 {
